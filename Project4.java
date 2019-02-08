0dacee3f59c01359a19cd774c0710506a8d373f7
russ1001 <30474446+russ1001@users.noreply.github.com>
2019-02-08 06:30:16 -0600

    Update Project4.java

--- Project4.java
+++ Project4.java
@@ -1,39 +1,67 @@
-import java.util.*;
-
-
-
-public class Project4 {
-	
-	static long start;
-	static long end;
-	static long total;
-	static int count;
-	String alphaString = "zyxwvutsrqponmlkjihgfedcba";
-	
-	public static void main(String[] args) {
-		
-		start = System.nanoTime();
-		System.out.println("Enter the string for encryption (minimun of 140 characters): ");
-		String message = "";
-		Scanner input = new Scanner(System.in);
-		message = input.next();
-		int checkLength = message.length();
-		
-		
-		
-		
-		int shiftKey = 0;
-		end = System.nanoTime();
-		total = end - start;
-		System.out.println(Math.round(total * .000000001) + Math.round(total * .000000001));		
-		
-		
-	}
-	
-	
-	//public static String encrypt(String encrypt, int shiftKey) {
-		
-		
-	//}
-
+import java.util.Scanner;
+ 
+public class TESTCipher
+{
+    static int start;
+	static int end;
+	static int total;
+	public static final String alphaString = "abcdefghijklmnopqrstuvwxyz";
+ 
+    
+    public static void main(String[] args)
+    {
+    	start = (int)System.currentTimeMillis();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the String for Encryption: ");
+        String message = new String();
+        message = input.nextLine();
+        
+        end = (int)System.currentTimeMillis();
+        total = end - start;
+        
+        long shiftKey1 = (Math.round(total * .001));
+        int shiftKey2 = (int)shiftKey1;
+        System.out.println(shiftKey1);
+        System.out.println(encrypt(message, shiftKey2));
+        System.out.println(decrypt(encrypt(message, 3), 3));
+        input.close();
+    }
+    
+    
+  
+    
+    public static String encrypt(String plainText, int shiftKey)
+    {
+        plainText = plainText.toLowerCase();
+        String cipherText = "";
+        for (int i = 0; i < plainText.length(); i++)
+        {
+            int charPosition = alphaString.indexOf(plainText.charAt(i));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = alphaString.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return cipherText;
+    }
+ 
+      
+    public static String decrypt(String cipherText, int shiftKey)
+    {
+        cipherText = cipherText.toLowerCase();
+        String plainText = "";
+        for (int i = 0; i < cipherText.length(); i++)
+        {
+            int charPosition = alphaString.indexOf(cipherText.charAt(i));
+            int keyVal = (charPosition - shiftKey) % 26;
+            if (keyVal < 0)
+            {
+                keyVal = alphaString.length() + keyVal;
+            }
+            char replaceVal = alphaString.charAt(keyVal);
+            plainText += replaceVal;
+        }
+        return plainText;
+    }
+ 
+    
 }
